ed8032157b5834ab71a72eb41484010a0ae9b075
Chandan N <49569507+chandangowda7@users.noreply.github.com>
2020-08-31 18:04:52 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,20 @@
 #include "stats.h"
+struct Stats
+{float average;
+ float max;
+ float min;
+};
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
+Stats Statistics::ComputeStatistics(const std::vector<float>& a) {
     //Implement statistics here
+    struct Stats s;
+    float sum=0;
+    for(i=0;i<a.size();i++)
+    {
+        sum=sum+a[i];
+     }
+    s.average=sum/a.size;
+    s.max=*max_element(a.begin(),a.end());
+    s.min=*min_element(a.begin(),a.end());
+    return s;
 }
