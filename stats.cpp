b16e015ae422da0dd9d90dc95c1b53ccb62e56d0
Chandan N <49569507+chandangowda7@users.noreply.github.com>
2020-08-31 18:59:44 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,5 @@
 #include "stats.h"
-class Statistics
+class Stats
 {float average;
  float max;
  float min;
@@ -7,7 +7,7 @@ class Statistics
 
 Stats Statistics::ComputeStatistics(const std::vector<float>& a) {
     //Implement statistics here
-    Statistics s;
+    Stats s;
     float sum=0,maxi=0,mini=99999;
     for(i=0;i<a.size();i++)
     {
