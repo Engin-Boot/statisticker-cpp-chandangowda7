7f6618b472c7c5916fd3889e8a7042d84038cb26
Chandan N <49569507+chandangowda7@users.noreply.github.com>
2020-09-01 14:59:03 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -2,7 +2,17 @@
 
 Stats Statistics::ComputeStatistics(const std::vector<float>& a) {
     //Implement statistics here
-    Stats s;
+     Stats s;
+    if(a.size()<0)
+    {
+         s.average=0.0;
+         s.max=0.0;
+         s.min=0.0;
+         s.isNan=true;
+        return s;
+    }
+        
+   
     float sum=a[0],maxi=a[0],mini=a[0];
     for(i=1;i<a.size();i++)
     {
@@ -19,5 +29,6 @@ Stats Statistics::ComputeStatistics(const std::vector<float>& a) {
     s.average=sum/a.size();
     s.max=maxi;
     s.min=mini;
+    s.isNan=false;
     return s;
 }
