d0836e500920f71bfea2f0481741ca189f8afeba
Chandan N <49569507+chandangowda7@users.noreply.github.com>
2020-09-01 15:55:51 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,20 +1,21 @@
 #include "stats.h"
-#include<limits>
+#include<cmath>
 
 Statistics::Stats Statistics::ComputeStatistics(const std::vector<float>& a) {
     //Implement statistics here
      Statistics::Stats s;
     if(a.size()<0)
     {
-         s.average=NaN();
-         s.max=NaN();
-         s.min=NaN();
+         s.average=nanf();
+         s.max=nanf();
+         s.min=nanf();
          
         return s;
     }
         
    
     float sum=a[0],maxi=a[0],mini=a[0];
+    int i;
     for(i=1;i<a.size();i++)
     {
      if(a[i]>maxi)
@@ -23,9 +24,7 @@ Statistics::Stats Statistics::ComputeStatistics(const std::vector<float>& a) {
      }
      if(a[i]<mini)
      mini=a[i];
-     
-     
-        sum=sum+a[i];
+    sum=sum+a[i];
      }
     s.average=sum/a.size();
     s.max=maxi;
