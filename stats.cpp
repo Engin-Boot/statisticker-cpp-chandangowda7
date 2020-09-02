1a925b7f8ca2fcab6ad81c7a43fcbe776aa8e783
Chandan N <49569507+chandangowda7@users.noreply.github.com>
2020-09-02 13:25:53 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -2,54 +2,59 @@
 #include<cmath>
 using namespace std;
 
-Statistics::Stats Statistics::ComputeStatistics(const std::vector<float>& a) {
+Statistics::Stats Statistics::ComputeStatistics(const std::vector<float>& arrayofNumbers) {
     //Implement statistics here
-     Statistics::Stats s;
-    if(a.size()==0)
+     Statistics::Stats stat{nanf(""),nanf(""),nanf("")};
+   if(!IsNaNPresent(arrayofNumbers))
+   {float sum=arrayofNumbers[0];//,maxi=a[0],mini=a[0];
+    int iterator;
+    for(iterator=1;iterator<arrayofNumbers.size();iterator++)
     {
-         s.average=nanf("");
-         s.max=nanf("");
-         s.min=nanf("");
-         
-        return s;
-    }
-   
-        
-   
-    float sum=a[0];//,maxi=a[0],mini=a[0];
-    int i;
-    for(i=1;i<a.size();i++)
-    {
-    sum=sum+a[i];
+    sum=sum+arrayofNumbers[iterator];
      }
-    s.average=sum/a.size();
-    s.max=maxElement(a);
-    s.min=minElement(a);
-   
+    s.average=sum/arrayofNumbers.size();
+    s.max=maxElement(arrayofNumbers);
+    s.min=minElement(arrayofNumbers);
+   }
     return s;
 }
-float Statistics::minElement(const std::vector<float>& a)
+float Statistics::minElement(const std::vector<float>& arrayofNumbers)
    {
-       float mini=a[0];
-    int i;
-       for(i=1;i<a.size();i++)
+       float min=arrayofNumbers[0];
+    int iterator;
+       for(iterator=1;iterator<arrayofNumbers.size();iterator++)
        {
-           if(a[i]<mini)
-               mini=a[i];
+           if(arrayofNumbers[iterator]<min)
+               min=arrayofNumbers[iterator];
        }
-       return mini;
+       return min;
        
    }
-float Statistics::maxElement(const std::vector<float>& a)
+float Statistics::maxElement(const std::vector<float>& arrayofNumbers)
    {
-       float max=a[0];
-    int i;
-       for(i=1;i<a.size();i++)
+       float max=arrayofNumbers[0];
+    int iterator;
+       for(iterator=1;iterator<arrayofNumbers.size();iterator++)
        {
-           if(a[i]>max)
-               max=a[i];
+           if(arrayofNumbers[iterator]>max)
+               max=arrayofNumbers[iterator];
        }
        return max;
        
    }
+ bool IsNaNPresent(const std::vector<float>& arrayofNumbers)
+ {
+      if(arrayofNumbers.size()==0)
+        return true;
+     else 
+     {
+         int iterator;
+         for(iterator=0;iterator<arrayofNumbers.size();iterator++)
+         {
+             if(std::isnan(arrayofNumbers[iterator]))
+                 return true;
+         }
+     }
+     return false;
+ }
 
